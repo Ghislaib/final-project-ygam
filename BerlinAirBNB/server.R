4cdf0b8f0fd548a10eb8ba40f9bc0daf9123cd08
Ghislain Bugingo <ghislainbugingo@D-10-19-19-6.dhcp4.washington.edu>
2018-12-01 15:44:23 -0800

    bettered the view

--- BerlinAirBNB/server.R
+++ BerlinAirBNB/server.R
@@ -1,27 +1,38 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
+library(shinydashboard)
+library(rgdal)
 
 df <- read.csv("listings.csv", stringsAsFactors = FALSE)
 
+berlin_data <- df %>% 
+  select(neighbourhood, host_name, price, reviews_per_month, room_type, minimum_nights)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  data <- reactive({
-    x <- df
-  })
   
-  output$mymap <- renderLeaflet({
-    df <- data()
-    m <- leaflet() %>%
-      addTiles() %>%
-      addMarkers(lng = df$longitude,
-                 lat = df$latitude,
-                 popup = paste("Average price:", df$price, "<br>",
-                               "Minimum nights:", df$minimum_nights, "<br>",
-                               "Number of reviews:", df$number_of_reviews, "<br>",
-                               "Host listing:", df$host_name, "<br>",
-                               "Availability:", df$availability_365))
 
-  })
+  data <- reactive({
+     x <- df
+   })
+   
+   output$mymap <- renderLeaflet({
+     df <- data() 
+     m <- leaflet() %>%
+       addTiles() %>%
+      addMarkers(lng = df$longitude,
+                  lat = df$latitude,
+                  popup = paste("Average price:", df$price, "<br>",
+                                "Minimum nights:", df$minimum_nights, "<br>",
+                                "Number of reviews:", df$number_of_reviews, "<br>",
+                                "Host listing:", df$host_name, "<br>",
+                                "Availability:", df$availability_365))
+
+   })
+   
+   output$berlin_data <- renderTable({
+     neighbourhoodFilter <- subset(berlin_data, berlin_data$neighbourhood == input$neighbourhood)
+   })
   
 })
--- BerlinAirBNB/ui.R
+++ BerlinAirBNB/ui.R
@@ -1,23 +1,52 @@
 library(shiny)
 library(dplyr)
 library(leaflet)
+library(shinydashboard)
+library(rgdal)
+library(DT)
 
 
 df <- read.csv("listings.csv", stringsAsFactors = FALSE)
 
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Map"),
-  
-  # Sidebar with a slider input for number of bins 
-  leafletOutput("mymap",height = 1000)
-  
-    )
-    
-    # Show a plot of the generated distribution
-    # mainPanel(
-    # )
+
+berlin_data <- df %>% 
+  select(neighbourhood, host_name, price, reviews_per_month, room_type)
+
+# # Define UI for application that draws a histogram
+# shinyUI(fluidPage(
+#   
+#   # Application title
+#   titlePanel("Berlin Airbnb"),
+#   
+#   # Sidebar with a slider input for number of bins 
+#   leafletOutput("mymap",height = 1000)
+#   
+#     )
+#     
+#     # Show a plot of the generated distribution
+#     # mainPanel(
+#     # )
+#   )
+
+
+ui <- dashboardPage(
+  skin = "red",
+  dashboardHeader(title = "Berlin Airbnb"),
+  dashboardSidebar(
+    selectInput("neighbourhood", "Select a neighbourhood", choices = berlin_data$neighbourhood),
+    sliderInput("price_range", label = "Price Range",
+                min = min(df$price),
+                max = max(df$price),
+                value = c(min(df$price), max(df$price)),
+                sep = "",
+                step = 1)
+    ),
+  dashboardBody(
+    fluidRow(box(width = 12, leafletOutput(outputId = "mymap"))),
+    fluidRow(box(width = 12, tableOutput(outputId = "berlin_data")))
   )
+
+)
+
+
 
